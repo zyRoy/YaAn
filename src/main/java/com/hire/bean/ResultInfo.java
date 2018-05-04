e9a41996501ab8c78f2dfd5dcdb31fa9bc8ee5e0
luoxq <965848837@qq.com>
2018-05-04 11:03:39 +0800

    初始化

---
+++ src/main/java/com/hire/bean/ResultInfo.java
@@ -0,0 +1,27 @@
+package com.hire.bean;
+
+/**
+ * Created by luoxq on 2018/4/25 0025.
+ */
+public class ResultInfo {
+
+    private String resultFlag;
+
+    private String resultInfo;
+
+    public String getResultFlag() {
+        return resultFlag;
+    }
+
+    public void setResultFlag(String resultFlag) {
+        this.resultFlag = resultFlag;
+    }
+
+    public String getResultInfo() {
+        return resultInfo;
+    }
+
+    public void setResultInfo(String resultInfo) {
+        this.resultInfo = resultInfo;
+    }
+}
